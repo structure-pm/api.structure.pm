64c8bd4d74a465a8d2fcfa610658dbafa0e2c449
Aaron Johnson <aarontropy@gmail.com>
2016-08-13 14:55:41 -0400

    add production config

--- src/config/development.js
+++ src/config/development.js
@@ -1,4 +1,13 @@
 const config = {
   dbPrefix: 'structudev',
+  db: {
+    connectionLimit: 100,
+    host: 'localhost',
+    port: 3306,
+    user: 'structu_atadmin',
+    password: '5trucm3',
+    database: undefined,
+    debug: false,
+  },
 }
 export default config;
--- src/config/production.js
+++ src/config/production.js
@@ -1,4 +1,13 @@
 const config = {
+  db: {
+    connectionLimit: 100,
+    host: 'localhost',
+    port: 3306,
+    user: 'structu_atadmin',
+    password: '5trucm3',
+    database: undefined,
+    debug: false,
+  },
   dbPrefix: 'structu',
 }
 export default config;
