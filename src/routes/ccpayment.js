17772b4c037cbcd4b511321d3f709e4e02f4d38b
Aaron Johnson <aarontropy@gmail.com>
2016-09-30 16:08:39 -0400

    add ping to ccpayment

--- src/routes/ccpayment.js
+++ src/routes/ccpayment.js
@@ -3,6 +3,9 @@ import * as cc from './controllers/ccpayment.controller';
 
 export default function(config) {
   let router = Router();
+  router.get('/ccpayment', function(req,res, next) {
+    res.json({msg: 'ccpayment pong'});
+  });
   router.post('/ccpayment', cc.makePayment);
   router.get('/ccpayment/methods', cc.getMethods);
 
