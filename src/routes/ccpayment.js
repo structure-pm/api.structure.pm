3beb2846c7e6382529e45f6dcc7f0c4605d850c4
Aaron Johnson <aarontropy@gmail.com>
2016-09-30 23:09:19 -0400

    added information to GET request

--- src/routes/ccpayment.js
+++ src/routes/ccpayment.js
@@ -4,7 +4,12 @@ import * as cc from './controllers/ccpayment.controller';
 export default function(config) {
   let router = Router();
   router.get('/ccpayment', function(req,res, next) {
-    res.json({msg: 'ccpayment pong'});
+    res.json({
+      service: 'ccpayment',
+      env: process.env.NODE_ENV,
+      demo: config.ccpayment.demo,
+      msg: 'pong'
+    });
   });
   router.post('/ccpayment', cc.makePayment);
   router.get('/ccpayment/methods', cc.getMethods);
