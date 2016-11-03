1cf6946804af7f956decbd6a71af753be74db219
Aaron Johnson <aarontropy@gmail.com>
2016-11-03 17:04:24 -0400

    accrual expenses sum on amount, not payment

--- src/domain/reports/dataServices/profitLoss_accrual.js
+++ src/domain/reports/dataServices/profitLoss_accrual.js
@@ -35,7 +35,7 @@ export function partitionBy(options, queryTypeSelector) {
     },
     expense: {
       prefix: 'el',
-      sumColumn: 'payment',
+      sumColumn: 'amount',
       defaultColSQL: data => `SUM(${data.prefix}.${data.sumColumn}) as ${data.field}`,
       timeColSQL: data => `SUM(CASE WHEN el.dueDate BETWEEN '${data.start}' AND '${data.end}' THEN ${data.prefix}.${data.sumColumn} ELSE 0 END) AS ${data.field}`,
       locColSQL: data => `SUM(CASE WHEN ${data.locSelector} THEN ${data.prefix}.${data.sumColumn} ELSE 0 END) AS ${data.field}`
