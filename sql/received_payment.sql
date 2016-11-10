4828abb9e9eccfff767d42935df912a7ed6c8a2e
Aaron Johnson <aarontropy@gmail.com>
2016-11-09 21:23:23 -0500

    update receivedPayment to include amount and num items

--- sql/received_payment.sql
+++ sql/received_payment.sql
@@ -1,10 +1,12 @@
 
-USE structudev_income;
+USE structutest_income;
 
 CREATE TABLE IF NOT EXISTS receivedPayment (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   leaseID INT,
   tenantID INT,
+  amount DECIMAL(10,2),
+  items INT,
   comment VARCHAR(500),
   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modifiedAt TIMESTAMP NOT NULL
--- src/domain/accounting/receivedPayment.js
+++ src/domain/accounting/receivedPayment.js
@@ -1,13 +1,21 @@
+import _pick from 'lodash/pick';
 import Income from './income';
 
 const ID_FIELD = 'id';
 const FIELDS = [
   ID_FIELD,
-  'leaseID', 'tenantID', 'comment', 'createdAt', 'modifiedAt',
+  'leaseID', 'tenantID', 'comment', 'amount', 'items', 'createdAt', 'modifiedAt',
 ];
+
+const defaults = {
+  amount: 0,
+  items: 0,
+}
 
 export default function ReceivedPayment(paymentData) {
   this.setLines(paymentData.lines || []);
+
+  Object.assign(this, _pick(paymentData, FIELDS));
 }
 
 ReceivedPayment.Fields = FIELDS;
@@ -40,13 +48,21 @@ ReceivedPayment.prototype.addLine = function(line) {
 ReceivedPayment.prototype.updateLine = function(lineIdx, line) {
   line._dirty = true;
   this._lines.splice(lineIdx, 1, line);
+  this._updateAggregates();
 }
 
 ReceivedPayment.prototype.removeLine = function(lineIdx) {
   const doomed = this._lines.splice(lineIdx, 1);
   this._deletedLines.push(doomed);
+  this._updateAggregates();
 }
 
 ReceivedPayment.prototype._addLine = function(line) {
   this._lines.push(line);
+  this._updateAggregates();
+}
+
+ReceivedPayment.prototype._updateAggregates = function() {
+  this.items = this.getLines().length;
+  this.amount = this.getLines().reduce((sum, l) => sum + l.amount, 0);
 }
