438e57be4727583569ff0b8ae4e70102ea265563
Aaron Johnson <aarontropy@gmail.com>
2016-11-13 22:25:00 -0500

    handle file uploads in file routes

--- src/routes/controllers/file.controller.js
+++ src/routes/controllers/file.controller.js
@@ -28,3 +28,35 @@ export function getFilesForAsset(req, res, next) {
 export function searchFiles(req, res, next) {
   next(new Error("not implemented"));
 }
+
+
+export function handleUpload(req, res, next) {
+
+  const missing = ['assetType', 'assetID', 'filename'].filter(fld => !req.query[fld] && !req.body[fld]);
+  if (missing.length) {
+    const err = new Error(`Missing required fileds from the querystring: ${missing.join(', ')}`);
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!res.files.upload.length) {
+    const err = new Error('Missing file. Files to be uploaded should use the `upload` field name in the request.');
+    err.status = 400;
+    return next(err);
+  }
+
+  const assetType   = req.query.assetType || req.body.assetType,
+        assetID     = req.query.assetID || req.body.assetID,
+        filename    = req.query.filename || req.body.filename,
+        description = req.query.description || req.body.description || 'Uploaded File',
+        title       = req.query.title || req.body.title || filename,
+        filebuffer  = req.files.upload[0].buffer,
+        mimeType    = req.file.upload[0].mimetype;
+
+
+  const gfileData = { assetType, assetID, filename, description, title, mimeType }
+
+  Assets.saveBufferToGFile(gfileData, filebuffer)
+    .then(result => res.json(result))
+    .catch(next);
+}
--- src/routes/file.js
+++ src/routes/file.js
@@ -1,11 +1,20 @@
+import multer from 'multer';
 import {Router} from 'express';
 import * as file from './controllers/file.controller';
+
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
 
 export default function(config) {
   let router = Router();
   router.get('/files/asset/:assetType/:assetID', file.getFilesForAsset);
   router.post('/files/search', file.searchFiles);
+
+  const fileFields = upload.fields([
+    { name: 'upload', maxCount: 1 },
+  ])
+  router.post('/scan/upload', fileFields, scan.handleScanUpload);
 
 
   return router;
